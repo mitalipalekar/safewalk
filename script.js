90fb53efa0f981bf0fc0e5ce377f7a3e19fb5adf
Christine Wolf <christinewolf@D-173-250-135-135.dhcp4.washington.edu>
2017-10-22 12:16:39 -0700

    added texting method

--- script.js
+++ script.js
@@ -108,3 +108,20 @@ function codeAddress() {
     }
   });
 }
+
+function sendSmsMessage(number, body) {
+  // number must be in a format similar to '+12345678901'
+
+  var accountSid = 'ACad44b176fb3ed197fd091c85a51cff0a'; // Your Account SID from www.twilio.com/console
+  var authToken = '2ba150009b6d22ba9b2e69d9d0003b49';   // Your Auth Token from www.twilio.com/console
+
+  var twilio = require('twilio');
+  var client = new twilio(accountSid, authToken);
+
+  client.messages.create({
+      body: body,
+      to: number,  // Text this number
+      from: '+12068006289' // From a valid Twilio number
+  })
+  .then((message) => console.log(message.sid));
+}
